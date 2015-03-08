e4fdbbdd73a4826a67a20667c1f1bc51bc93d2e4
Gabriel Furini <gabrielfurini@gmail.com>
2015-03-08 18:13:06 -0300

    Renaming file

--- Node.js/nodeschool.io/3-my-fist-io.js
+++
@@ -1,9 +0,0 @@
-// Count the number of new lines of an archive using fs module
-// the first command-line parameter is the file's path
-
-var fs = require('fs'),
-	pathToFile = process.argv[2],
-	buffer = fs.readFileSync(pathToFile).toString(),
-	newLines = buffer.split('\n').length - 1;
-
-console.log(newLines);
